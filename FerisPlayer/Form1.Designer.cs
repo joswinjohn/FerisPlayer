f59ab0657e814aea7362922464989fa239bbfc72
joswinjohn <65504682+joswinjohn@users.noreply.github.com>
2021-03-03 20:47:06 -0500

    First Build

---
+++ FerisPlayer/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace FerisPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.looptimer = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.autoplaytimer = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.BackColor = System.Drawing.Color.Coral;
+            this.PlayButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.PlayButton.FlatAppearance.BorderSize = 0;
+            this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PlayButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.PlayButton.Location = new System.Drawing.Point(29, 457);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(216, 57);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "▶";
+            this.PlayButton.UseVisualStyleBackColor = false;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.BackColor = System.Drawing.Color.Coral;
+            this.PauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PauseButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PauseButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.PauseButton.Location = new System.Drawing.Point(251, 457);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(212, 57);
+            this.PauseButton.TabIndex = 1;
+            this.PauseButton.Text = "⏸️";
+            this.PauseButton.UseVisualStyleBackColor = false;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(29, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(434, 315);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(177, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Loop";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // looptimer
+            // 
+            this.looptimer.Interval = 1000;
+            this.looptimer.Tick += new System.EventHandler(this.looptimer_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button2.Location = new System.Drawing.Point(100, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "⏪";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Coral;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button3.Location = new System.Drawing.Point(313, 411);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "⏩";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // autoplaytimer
+            // 
+            this.autoplaytimer.Tick += new System.EventHandler(this.autoplay_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Coral;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button4.Location = new System.Drawing.Point(29, 411);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "⏮";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Coral;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button5.Location = new System.Drawing.Point(390, 411);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 40);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "⏭";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(29, 360);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(371, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.textBox1.Location = new System.Drawing.Point(413, 365);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 31);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "0:00 / 0:00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(483, 526);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.PlayButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Feris";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer looptimer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer autoplaytimer;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ FerisPlayer/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace FerisPlayer
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            wplayer.URL = $"./Music/{listBox1.SelectedItem}";
+            wplayer.controls.stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Functions.PopulateListBox(listBox1, "./Music", "*.mp3");
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Music";
+            wplayer.newPlaylist("pMain", path);
+        }
+
+        string dur = "0:00";
+        string cdur = "0:00";
+        public void PlayButton_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.play();
+            autoplaytimer.Enabled = true;
+
+            timer3.Enabled = true;
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.pause();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (looptimer.Enabled == true)
+            {
+                looptimer.Enabled = false;
+            } else
+            {
+                looptimer.Enabled = true;
+            }
+        }
+
+        private void looptimer_Tick(object sender, EventArgs e)
+        {
+            if (autoplaytimer.Enabled)
+            {
+                wplayer.settings.setMode("loop", true);
+            } else
+            {
+                wplayer.settings.setMode("loop", false);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.fastReverse();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.fastForward();
+        }
+
+        private void autoplay_Tick(object sender, EventArgs e)
+        {
+	        if (looptimer.Enabled == false && wplayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                int index = listBox1.SelectedIndex + 1;
+                listBox1.SetSelected(index, true);
+                wplayer.controls.play();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex - 1;
+            listBox1.SetSelected(index, true);
+            wplayer.controls.play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex + 1;
+            listBox1.SetSelected(index, true);
+            wplayer.controls.play();
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            dur = wplayer.currentMedia.durationString;
+            cdur = wplayer.controls.currentPositionString;
+            trackBar1.Maximum = Convert.ToInt32(wplayer.currentMedia.duration);
+            
+            textBox1.Text = cdur + " / " + dur;
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            trackBar1.Value = Convert.ToInt32(wplayer.controls.currentPosition);
+        }
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                wplayer.controls.pause();
+                timer4.Enabled = false;
+                wplayer.controls.currentPosition = trackBar1.Value;
+            } else
+            {
+                timer4.Enabled = true;
+            }
+
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            wplayer.controls.currentPosition = trackBar1.Value;
+            timer4.Enabled = true;
+            wplayer.controls.play();
+        }
+    }
+}
